c3762ab3ab1e136d453d34f32fd375b999be41f1
Jonathan De Moor <jonathan@flowcaptain.com>
2013-01-14 21:19:04 +0100

    clean up config file

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,18 +1,13 @@
 <?php
 
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
-
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Jonah CMS',
 
-	// preloading 'log' component
-	'preload'=>array(	'log',
-						'bootstrap'
-						),
+	'preload'=>array(
+		'log',
+		'bootstrap'
+	),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -20,20 +15,12 @@ return array(
 		'application.components.*',
 		'application.helpers.*',
 	),
+	
 	'aliases' => array(
 		'xupload' => 'ext.xupload'
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
 		'gii'=>array(
 	        'generatorPaths'=>array(
 	            'bootstrap.gii',
@@ -44,7 +31,6 @@ return array(
 	// application components
 	'components'=>array(
 		'user'=>array(
-			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
 		'bootstrap'=>array(
@@ -58,13 +44,9 @@ return array(
 	    ),
 	    'image'=>array(
           'class'=>'application.extensions.image.CImageComponent',
-            // GD or ImageMagick
             'driver'=>'GD',
-            // ImageMagick setup path
             'params'=>array('directory'=>'/opt/local/bin'),
-        ),
-		// uncomment the following to enable URLs in path-format
-		
+        ),		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'showScriptName' => false,
@@ -76,19 +58,8 @@ return array(
 			),
 		),
 		
-		
 		'db'=> include(dirname(__FILE__) . '/db.php'),
 		
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -100,17 +71,9 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
 			),
 		),
 	),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
 	'params' => include(dirname(__FILE__) . '/params.php'),
 );

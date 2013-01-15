7afbe16f37d5b0e2e8ef1b3c66a4b8d933f2cbe7
Jonathan De Moor <jonathan@flowcaptain.com>
2013-01-15 22:37:42 +0100

    Add Page model

---
+++ protected/models/Page.php
@@ -0,0 +1,62 @@
+<?php
+
+class Page extends ApplicationModel {
+        
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
+    
+    public function tableName() {
+        return 'tbl_pages';
+    }
+    
+    public function rules() {
+        return array(
+            array(
+                'name',
+                'required'
+            ),
+        );
+    }
+    
+    public function attributeLabels() {
+        return array(
+            'name' => 'Name',
+        );
+    }
+    
+    public function findByID($id) {
+        return $this->find(
+            'id = :id',
+            array('id' => $id)
+        );
+    }
+    
+    public function findAllForSelect() {
+	    $pages = $this->findAll();
+	    
+	    $result = array();
+	    
+	    // Loop over pages
+	    foreach($pages as $page) {
+		    $item = array();
+		    $item['label'] = $page->name;
+		    $item['url'] = array('contentBlock/admin/filter/' . $page->id);
+		    
+		    $result[] = $item;
+	    }
+	    
+	    // Add separator
+	    $result[] = '---';
+		
+		// Add Reset btn    
+		$item = array();
+	    $item['label'] = 'All Pages';
+	    $item['url'] = array('contentBlock/admin');
+	    $item['active'] = false;
+	    
+	    $result[] = $item;
+	    
+	    return $result;
+    }    
+}

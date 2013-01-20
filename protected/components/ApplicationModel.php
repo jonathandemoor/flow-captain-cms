29e5abc3098cd85935347516c7c1aca18f491d6a
Jonathan De Moor <jonathan@flowcaptain.com>
2013-01-20 16:15:03 +0100

    Update AppModel - exception handling

--- protected/components/ApplicationModel.php
+++ protected/components/ApplicationModel.php
@@ -2,33 +2,36 @@
 
 class ApplicationModel extends CActiveRecord {
    
-	public function save($runValidation = true, $attributes = null) {
-		
+	public function save($runValidation = true, $attributes = null) {		
 		// Check if is new record
 		if ($this->isNewRecord) {
 			// Set created time
-			$this->created_on = new CDbExpression('NOW()');
+			try {
+				$this->created_on = new CDbExpression('NOW()');
+			} catch (Exception $e) {
+				Yii::log('Failed to insert created_on', 'error', 'AppModel');				
+			}
 		}
 		
-		
-		
-		// For updated records, set the updated fields
+		// Updating record
 		try {
 			// Set updated time
-			$this->updated_on = new CDbExpression('NOW()');
-
-			// This doesn't work for console applications
-			try {
-				$user = User::model()->findActiveByEmail(Yii::app()->user->name);
-				$this->updated_by = $user->fullname;
-				
-			} catch (Exception $e) {				
-			}
+			$this->updated_on = new CDbExpression('NOW()');			
 
 		} catch (Exception $e) {
+			Yii::log('Failed to update update_on', 'error', 'AppModel');
 		}
-
+		
+		// Updating record
+		try {
+			// Set updated user
+			$user = User::model()->findActiveByEmail(Yii::app()->user->name);
+			$this->updated_by = $user->fullname;
+			
+		} catch (Exception $e) {
+			Yii::log('Failed to update update_on', 'error', 'AppModel');				
+		}
+		
 		return parent::save($runValidation, $attributes);
-	}
-	
+	}	
 }

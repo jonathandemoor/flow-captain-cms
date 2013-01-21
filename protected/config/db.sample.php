84e3a115196219114c2ad11c1d8c9cdb840595a0
Jonathan De Moor <jonathan@flowcaptain.com>
2013-01-21 22:04:49 +0100

    Sample DB file

---
+++ protected/config/db.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'connectionString'      => 'mysql:host=hostUrl;dbname=dbName',
+    'charset'               => 'UTF8',
+    'username'              => 'user',
+    'password'              => 'pass',
+    'enableParamLogging'    => false,
+    'schemaCachingDuration' => 0,
+);
+
